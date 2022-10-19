465897f8671702935305d2b52f9084d7041ead6a
Omar-Codess <omartaki8@gmail.com>
2022-10-19 18:24:11 +0200

    scritto pseudo codice

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Faccio generare al pc 5 numeri casuali
+    // -Math.random
+
+// Li stampo in console
+    // -console.log
+
+// Li stampo in pagina
+    // innerHTML
+
+// Faccio partire un timer di 30 secondi
+    // funzione setInterval
+
+// Al termine dei 30 secondi faccio sparire i numeri
+    // funzione clearInterval
+
+// Chiedo all'utente di inserire i numeri che ha visto uno alla volta
+    // prompt "inserisci uno dei cinque numeri che hai appena visto"
+
+// Dico all'utente quanti e quali numeri ha azzeccato
+    // if userNumber === a uno di quelli nell'array dei numeri casuali 
+    //     hai indovinato questo numero
+    // else 
+    //     hai sbagliato
